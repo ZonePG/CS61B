85bfd0e5928e182743b2750d3babb0b4b8cf5bb0
ZonePG <zonepg666@gmail.com>
2022-03-21 02:02:09 +0800

    done for lab4b

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,12 +5,10 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
+        if (n % 2 == 1) {
             return 3 * n + 1;
         } else {
-            return n * 2;
+            return n / 2;
         }
     }
 
